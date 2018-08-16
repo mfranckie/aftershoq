a24180648a52efeb00bc516baf9febe859ed222e
martin <martin.franckie@gmail.com>
2018-08-16 11:39:08 +0200

    Added two mid-IR QCLs.

--- utils/qcls.py
+++ utils/qcls.py
@@ -38,7 +38,7 @@ class EV2416(Structure):
         
 class EV1157(Structure):
     '''
-    Amanit et al. New J. Phys. 11, 125022 (2009)
+    Amanti et al., New J. Phys. 11, 125022 (2009)
     '''
     
     def __init__(self):
@@ -61,3 +61,77 @@ class EV1157(Structure):
         vdop = 2e16 # cm^-3
         
         self.addDoping(0, self.layers[idop].width, vdop, idop)
+        
+class EV1907(Structure):
+    '''
+    Strained 8.5 micron design. Published in thesis of J. Wolf (ETH Zuerich, 2017)
+    '''
+    
+    def __init__(self):
+        Structure.__init__(self)
+        
+        self.setIFR(0.1, 10)
+        
+        alinas_s = AlInAs(x = 0.64)
+        gainas_s = InGaAs(x = 0.58)
+        
+        
+        self.addLayerMW(3.1, alinas_s)
+        self.addLayerMW(2.5, gainas_s)
+        self.addLayerMW(0.6, alinas_s)
+        self.addLayerMW(5.7, gainas_s)
+        self.addLayerMW(0.7, alinas_s)
+        self.addLayerMW(5.5, gainas_s)
+        self.addLayerMW(1.2, alinas_s)
+        self.addLayerMW(4.6, gainas_s)
+        self.addLayerMW(1.1, alinas_s)
+        self.addLayerMW(4.5, gainas_s) # <----- #9  Doped to 0.120276*10^18 cm^-3
+        self.addLayerMW(1.4, alinas_s) # <----- #10 Doped to 0.120276*10^18 cm^-3 
+        self.addLayerMW(4.0, gainas_s) # <----- #11 Doped to 0.120276*10^18 cm^-3
+        self.addLayerMW(1.5, alinas_s)
+        self.addLayerMW(3.4, gainas_s)
+        self.addLayerMW(1.7, alinas_s)
+        self.addLayerMW(3.5, gainas_s)
+        
+        dop = 0.10101e18
+        
+        self.addDoping(0, 4.5, dop, 9)
+        self.addDoping(0, 1.4, dop, 10)
+        self.addDoping(0, 4.0, dop, 11)
+        
+class EV2017(Structure):
+    '''
+    Genetically optimized 8.5 micron design. Published in thesis of J. Wolf (ETH Zuerich, 2017)
+    '''
+    
+    def __init__(self):
+        Structure.__init__(self)
+        
+        self.setIFR(0.1, 10)
+        
+        alinas_s = AlInAs(x = 0.64)
+        gainas_s = InGaAs(x = 0.58)
+        
+        
+        self.addLayerMW(3.1, alinas_s)
+        self.addLayerMW(2.52, gainas_s)
+        self.addLayerMW(1.23, alinas_s)
+        self.addLayerMW(5.77, gainas_s)
+        self.addLayerMW(0.74, alinas_s)
+        self.addLayerMW(5.0, gainas_s)
+        self.addLayerMW(1.01, alinas_s)
+        self.addLayerMW(4.49, gainas_s)
+        self.addLayerMW(1.27, alinas_s)
+        self.addLayerMW(3.79, gainas_s) # <----- #9  Doped to 0.120276*10^18 cm^-3
+        self.addLayerMW(1.29, alinas_s) # <----- #10 Doped to 0.120276*10^18 cm^-3 
+        self.addLayerMW(3.23, gainas_s) # <----- #11 Doped to 0.120276*10^18 cm^-3
+        self.addLayerMW(1.60, alinas_s)
+        self.addLayerMW(2.89, gainas_s)
+        self.addLayerMW(1.89, alinas_s)
+        self.addLayerMW(3.01, gainas_s)
+        
+        dop = 0.120276e18
+        
+        self.addDoping(0, 3.79, dop, 9)
+        self.addDoping(0, 1.29, dop, 10)
+        self.addDoping(0, 3.23, dop, 11)
